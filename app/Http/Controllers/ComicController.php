3904eb481a6ff960c555a718afe9909786bb8cfa
matteotagliatti <matteotagliatti@gmail.com>
2022-05-17 15:35:01 +0200

    controller done

---
+++ app/Http/Controllers/ComicController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Comic;
+
+use Illuminate\Http\Request;
+
+class ComicController extends Controller
+{
+    public function index()
+    {
+        $comics = Comic::paginate(10);
+        $data = ["comics" => $comics];
+        return view('comics.index', $data);
+    }
+
+    /* public function show($id) {
+        $comic = Comic::findOrFail($id);
+        $data = ["comic" => $comic];
+        return view('comics.show', $data);
+    } */
+}
